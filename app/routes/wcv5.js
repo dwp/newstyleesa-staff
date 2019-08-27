8ad6991ac3414150ead8182eeddee2174154047b
Phil Swan <mail@philswan.co.uk>
2019-08-27 11:58:03 +0100

    Tidy up

--- app/routes/wcv5.js
+++ app/routes/wcv5.js
@@ -249,34 +249,12 @@ module.exports = function (app) {
     // However in JavaScript we can't use hyphens in variable names
 
     let status = req.session.data['status']
-
     let nino = req.session.data['nino']
-
     let early = req.session.data['early']
 
-    // Verified
-    if (status === 'verified') {
-      res.redirect(`upload?status=appointmentbooked&nino=${nino}&early=${early}`)
-    // Failed to attend
-    } else if (status === 'fta') {
-      res.redirect(`status-confirmation?status=fta&nino=${nino}&early=${early}`)
-    // Could not be verified
-    } else if (status === 'not-verified') {
-      res.redirect(`not-verified-reason?status=appointmentbooked&nino=${nino}&early=${early}`)
-    // New appointment needed (is this still a thing?)
-    } else if (status === 'newappointmentneeded') {
-      res.redirect(`status-confirmation?status=newappointmentneeded&nino=${nino}&early=${early}`)
-    // Appointment booked
-    } else if (status === 'appointmentbooked') {
-      res.redirect(`status-confirmation?status=appointmentbooked&nino=${nino}&early=${early}`)
     // New appointment booked
-    } else if (status === 'newappointmentbooked') {
+    if (status === 'newappointmentbooked') {
       res.redirect(`status-confirmation?status=newappointmentbooked&nino=${nino}&early=${early}`)
-    // Withdrawn or duplicate
-    } else if (status === 'withdrawn') {
-      res.redirect(`withdrawn-confirmation?status=appointmentbooked&nino=${nino}`)
-    // } else if (status === 'withdrawn') {
-    //   res.redirect(`status-confirmation?status=withdrawn&nino=${nino}&early=${early}`)
     // Processed
     } else if (status === 'processed') {
       res.redirect(`notes-processed?status=verified&nino=${nino}`)
